d6a4b799386f291e511c60289fea45186b95c7dc
Ren <sren@friendsbalt.org>
2022-05-19 09:32:46 -0400

    .

--- source.js
+++ source.js
@@ -5,14 +5,9 @@ function handleResponse(response){
     for(let i = 0; i < numResults; i++){
         let book = response.items[i];
         console.log(book.volumeInfo.title);
-        para.innerHTML += "<br> <h3>TITLE:</h3> " + book.volumeInfo.title;
+        para.innerHTML += "<h3>TITLE:</h3> " + book.volumeInfo.title;
         para.innerHTML += "<br> <h3>AUTHOR:</h3> " + book.volumeInfo.authors;
         para.innerHTML += "<br> <h3>DESCRIPTION:</h3> " + book.volumeInfo.description;
-        para.innerHTML += "<br>";
+        para.innerHTML += "<br><br>";
     }
 }
-
-const api_url = "https://www.googleapis.com/books/v1/volumes?q=";
-let queries = "java&callback=handleResponse";
-
-src = api_url + queries;
