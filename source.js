6565168aa1ef348e1b2ec022e1d132b31b0f76cd
Ren <sren@friendsbalt.org>
2022-05-20 15:18:36 -0400

    .

--- source.js
+++ source.js
@@ -7,18 +7,28 @@ function handleResponse(response){
         console.log(book.volumeInfo.title);
 
         let para = document.createElement("p");
-        para.innerHTML += "<h3>TITLE:</h3> " + book.volumeInfo.title;
+        para.innerHTML += "<h2>" + book.volumeInfo.title + "</h2>";
         para.innerHTML += "<br> <h3>AUTHOR:</h3> " + book.volumeInfo.authors;
         para.innerHTML += "<br> <h3>DESCRIPTION:</h3> " + book.volumeInfo.description;
         let img = document.createElement("img");
         img.src = book.volumeInfo.imageLinks.thumbnail;
 
-        let btn = document.createElement("button");
-        btn.innerHTML = "Buy";
-        btn.src = book.volumeInfo.buyLink;
-
         results.appendChild(img);
         results.appendChild(para);
+
+        let btn = document.createElement("button");
+        btn.innerHTML = "Buy";
+        if(book.saleInfo.saleability == "FOR_SALE"){
+            let a = document.createElement('a'); 
+            let link = document.createTextNode("This is link");
+            results.appendChild(link); 
+            a.href = book.saleInfo.buyLink; 
+            results.appendChild(a);
+            //btn.href = (book.saleInfo.buyLink);
+        }
+        else{
+            btn.disabled = true;
+        }
         results.appendChild(btn);
     }
 }
