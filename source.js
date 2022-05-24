672373ac0aa6e022387c235019965fbe3e647cf9
Ren <sren@friendsbalt.org>
2022-05-24 10:55:09 -0400

    .

--- source.js
+++ source.js
@@ -1,10 +1,21 @@
 let results = document.getElementById("library");
 let numResults = 3;
 
+function search(){
+    let query = document.getElementById("searchbar").value;
+    console.log(query);
+    document.getElementById("query").innerHTML = query;
+    query.replace(" ", "+");
+    numResults = document.getElementById("num").value;
+    let s = document.getElementById("api-script");
+    s.src="https://www.googleapis.com/books/v1/volumes?q=" + query + "&callback=handleResponse";
+    console.log(query);
+    document.location.reload();
+}
+
 function handleResponse(response){
     for(let i = 0; i < numResults; i++){
         let book = response.items[i];
-        console.log(book.volumeInfo.title);
 
         let para = document.createElement("p");
         para.innerHTML += "<h2>" + book.volumeInfo.title + "</h2>";
@@ -17,16 +28,16 @@ function handleResponse(response){
         results.appendChild(para);
 
         let btn = document.createElement("button");
-        btn.innerHTML = "Buy";
         if(book.saleInfo.saleability == "FOR_SALE"){
             let a = document.createElement('a'); 
-            let link = document.createTextNode("This is link");
-            results.appendChild(link); 
-            a.href = book.saleInfo.buyLink; 
-            results.appendChild(a);
-            //btn.href = (book.saleInfo.buyLink);
+            let link = document.createTextNode("Buy");
+            a.appendChild(link); 
+            a.href = book.saleInfo.buyLink;
+
+            btn.appendChild(a);
         }
         else{
+            btn.innerHTML = "Buy";
             btn.disabled = true;
         }
         results.appendChild(btn);
